5da55f61f0b9345508e414473f3358253fc54635
covereddragon3 <168766182+covereddragon3@users.noreply.github.com>
2024-11-21 14:16:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'mamga',
+    'qual foi o manga mais vendido em 2018',
+    'one piece'
+)
+
+criaCartao(
+    'jogo',
+    'qual e o jogo mais vendido em 2018',
+    'red dead redemption 2'
+)
+
+criaCartao(
+    'filme',
+    'quando o filme tron foi feito',
+    '2010'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz cachorro em Inglês?',
+    'cachorro em ingles é dog'
+)
